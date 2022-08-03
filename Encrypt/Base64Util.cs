51eed2f7ba201cb78f3e9ae1c897e04340e99fee
WALLE <wenguoli_520@qq.com>
2022-08-03 11:39:17 +0800

    update

--- Encrypt/Base64Util.cs
+++ Encrypt/Base64Util.cs
@@ -8,7 +8,7 @@
 *版本号： V1.0.0.0
 *唯一标识：f9221ea6-a9c7-4289-829a-6b2bff190a6f
 *当前的用户域：WALLE
-*创建人： yswen
+*创建人： yswenli
 *电子邮箱：walle.wen@tjingcai.com
 *创建时间：2022/7/29 11:30:29
 *描述：
@@ -16,7 +16,7 @@
 *=================================================
 *修改标记
 *修改时间：2022/7/29 11:30:29
-*修改人： yswen
+*修改人： yswenli
 *版本号： V1.0.0.0
 *描述：
 *
@@ -49,6 +49,53 @@ namespace Encrypt.Library
         public static string ConvertToUTF8Str(this string base64Str)
         {
             return Encoding.UTF8.GetString(Convert.FromBase64String(base64Str));
+        }
+
+        /// <summary>
+        /// UriSafe加密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string UriSafeEncode(byte[] input)
+        {
+            if (input is null)
+                throw new ArgumentNullException(nameof(input));
+            if (input.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(input));
+
+            var output = Convert.ToBase64String(input);
+            output = output.Split('=')[0];
+            output = output.Replace('+', '-');
+            output = output.Replace('/', '_');
+            return output;
+        }
+        /// <summary>
+        /// UriSafe解密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static byte[] UriSafeDecode(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentException(nameof(input));
+
+            var output = input;
+            output = output.Replace('-', '+');
+            output = output.Replace('_', '/');
+            switch (output.Length % 4)
+            {
+                case 0:
+                    break;
+                case 2:
+                    output += "==";
+                    break;
+                case 3:
+                    output += "=";
+                    break;
+                default:
+                    throw new FormatException("非法 base64url 字符串。");
+            }
+            return Convert.FromBase64String(output);
         }
     }
 }
