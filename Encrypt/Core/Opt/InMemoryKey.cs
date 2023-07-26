d7cc9d4f665d57adb9454b042771c0256e1613df
WALLE <yswenli@outlook.com>
2023-07-26 18:06:58 +0800

    增加opt相关算法

---
+++ Encrypt/Core/Opt/InMemoryKey.cs
@@ -0,0 +1,143 @@
+﻿/*
+Credits to Devin Martin and the original OtpSharp library:
+https://bitbucket.org/devinmartin/otp-sharp/overview 
+
+Copyright (C) 2012 Devin Martin
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Security.Cryptography;
+
+namespace Encrypt.Library.Core.Opt
+{
+
+    /// <summary>
+    /// Represents a key in memory
+    /// </summary>
+    /// <remarks>
+    /// This will attempt to use the Windows data protection api to encrypt the key in memory.
+    /// However, this type favors working over memory protection. This is an attempt to minimize
+    /// exposure in memory, nothing more. This protection is flawed in many ways and is limited
+    /// to Windows.
+    /// 
+    /// In order to use the key to compute an hmac it must be temporarily decrypted, used,
+    /// then re-encrypted. This does expose the key in memory for a time. If a memory dump occurs in this time
+    /// the plaintext key will be part of it. Furthermore, there are potentially
+    /// artifacts from the hmac computation, GC compaction, or any number of other leaks even after
+    /// the key is re-encrypted.
+    /// 
+    /// This type favors working over memory protection. If the particular platform isn't supported then,
+    /// unless forced by modifying the IsPlatformSupported method, it will just store the key in a standard
+    /// byte array.
+    /// </remarks>
+    public class InMemoryKey : IKeyProvider
+    {
+        private readonly object _stateSync = new object();
+        private readonly byte[] _keyData;
+        private readonly int _keyLength;
+
+        /// <summary>
+        /// Creates an instance of a key.
+        /// </summary>
+        /// <param name="key">Plaintext key data</param>
+        public InMemoryKey(byte[] key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (key.Length <= 0)
+            {
+                throw new ArgumentException("The key must not be empty");
+            }
+
+            _keyLength = key.Length;
+            var paddedKeyLength = (int)Math.Ceiling((decimal)key.Length / (decimal)16) * 16;
+            _keyData = new byte[paddedKeyLength];
+            Array.Copy(key, _keyData, key.Length);
+        }
+
+        /// <summary>
+        /// Gets a copy of the plaintext key
+        /// </summary>
+        /// <remarks>
+        /// This is internal rather than protected so that the tests can use this method
+        /// </remarks>
+        /// <returns>Plaintext Key</returns>
+        internal byte[] GetCopyOfKey()
+        {
+            var plainKey = new byte[_keyLength];
+            lock (_stateSync)
+            {
+                Array.Copy(_keyData, plainKey, _keyLength);
+            }
+            return plainKey;
+        }
+
+        /// <summary>
+        /// Uses the key to get an HMAC using the specified algorithm and data
+        /// </summary>
+        /// <param name="mode">The HMAC algorithm to use</param>
+        /// <param name="data">The data used to compute the HMAC</param>
+        /// <returns>HMAC of the key and data</returns>
+        public byte[] ComputeHmac(OtpHashMode mode, byte[] data)
+        {
+            byte[] hashedValue;
+            using (var hmac = CreateHmacHash(mode))
+            {
+                var key = GetCopyOfKey();
+                try
+                {
+                    hmac.Key = key;
+                    hashedValue = hmac.ComputeHash(data);
+                }
+                finally
+                {
+                    KeyUtilities.Destroy(key);
+                }
+            }
+
+            return hashedValue;
+        }
+
+        /// <summary>
+        /// Create an HMAC object for the specified algorithm
+        /// </summary>
+        private static HMAC CreateHmacHash(OtpHashMode otpHashMode)
+        {
+            HMAC hmacAlgorithm;
+            switch (otpHashMode)
+            {
+                case OtpHashMode.Sha256:
+                    hmacAlgorithm = new HMACSHA256();
+                    break;
+                case OtpHashMode.Sha512:
+                    hmacAlgorithm = new HMACSHA512();
+                    break;
+                // case OtpHashMode.Sha1:
+                default:
+                    hmacAlgorithm = new HMACSHA1();
+                    break;
+            }
+            return hmacAlgorithm;
+        }
+    }
+}
---
+++ Encrypt/OptUtil.cs
@@ -0,0 +1,79 @@
+﻿/****************************************************************************
+*Copyright (c) 2023 RiverLand All Rights Reserved.
+*CLR版本： 4.0.30319.42000
+*机器名称：WALLE
+*公司名称：RiverLand
+*命名空间：Encrypt.Library
+*文件名： OptUtil
+*版本号： V1.0.0.0
+*唯一标识：66127935-3a06-4125-bcc1-4e678a739ae0
+*当前的用户域：WALLE
+*创建人： yswenli
+*电子邮箱：walle.wen@tjingcai.com
+*创建时间：2023/7/26 17:51:40
+*描述：
+*
+*=================================================
+*修改标记
+*修改时间：2023/7/26 17:51:40
+*修改人： yswenli
+*版本号： V1.0.0.0
+*描述：
+*
+*****************************************************************************/
+using System.Text;
+
+using Encrypt.Library.Core.Opt;
+
+namespace Encrypt.Library
+{
+    public static class OptUtil
+    {
+        /// <summary>
+        /// 获取totp计算值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="size"></param>
+        /// <param name="otpHashMode"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static string GetTotp(string key, int size = 6, OtpHashMode otpHashMode = OtpHashMode.Sha1, int seconds = 30)
+        {
+            var otp = new Totp(Encoding.UTF8.GetBytes(key), seconds, otpHashMode, size);
+            return otp.ComputeTotp();
+        }
+
+        /// <summary>
+        /// 获取hotp计算值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="counter"></param>
+        /// <param name="otpHashMode"></param>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static string GetHotp(string key, long counter, OtpHashMode otpHashMode = OtpHashMode.Sha1, int seconds = 30)
+        {
+            var otp = new Hotp(Encoding.UTF8.GetBytes(key), otpHashMode, seconds);
+            return otp.ComputeHOTP(counter);
+        }
+
+
+        /// <summary>
+        /// 获取optUri
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <param name="otpType"></param>
+        /// <param name="user"></param>
+        /// <param name="issuer"></param>
+        /// <param name="hash"></param>
+        /// <param name="digits"></param>
+        /// <param name="period"></param>
+        /// <param name="counter"></param>
+        /// <returns></returns>
+        public static string GetOtpUri(string secret, OtpType otpType, string user, string issuer,
+        OtpHashMode hash, int digits, int period, int counter)
+        {
+            return new OtpUri(otpType, secret, user, issuer, hash, digits, period, counter).ToString();
+        }
+    }
+}
